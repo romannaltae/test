ba63c5a154330ace56dba73343da4137e0c18b9f
Dir <ooo_31@bk.ru>
2023-07-03 15:04:12 +0700

    test2

--- tina/config.tsx
+++ tina/config.tsx
@@ -8,10 +8,7 @@ import { iconSchema } from "../components/util/icon";
 
 const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
-  branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
-    process.env.HEAD!, // Netlify branch env
+  branch: "main",
   token: process.env.TINA_TOKEN!,
   media: {
      loadCustomStore: async () => {
